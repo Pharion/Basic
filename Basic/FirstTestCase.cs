c5a77980b8b5ef9d01f8a48e027a7c61baae68b5
Stephen <pharion3d@gmail.com>
2023-07-24 11:44:43 +0100

    Add project files.

---
+++ Basic/FirstTestCase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace Basic
+{
+    [TestClass]
+    public class FirstTestCase
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://www.bbc.co.uk";
+
+        }
+    }
+}
---
+++ Basic/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Basic")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Basic")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6c5de616-99d0-42bf-b51a-2909ece98fca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
